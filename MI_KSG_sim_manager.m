3b0f6d5b270aac2807b14d09821134e9e43bdeb1
neuRowsATL <bchung808@gmail.com>
2018-12-14 15:38:58 -0500

    updated sim_manager class

--- MI_KSG_sim_manager.m
+++ MI_KSG_sim_manager.m
@@ -1,18 +1,38 @@
 classdef MI_KSG_sim_manager
     properties
-        verbose; % set level of output for reference and debugging
-        sim_params; % array of simulation data and k-values
-        sim_data; % matrix of mutual information calculation points
+        verbose % set level of output for reference and debugging
+        sim_params % array of simulation data and k-values
+        sim_data % matrix of mutual information calculation points
+        thread_count % number of available cores for parallel
+        par_mode % flag to run in parallel mode
     end
     methods
-        function obj = MI_KSG_sim_manager(verbose)
+        function obj = MI_KSG_sim_manager(mode, verbose)
             if nargin == 1
+                obj.par_mode = mode;
+                obj.verbose = 0;
+            elseif nargin == 2
+                obj.par_mode = mode;
                 obj.verbose = verbose;
+            elseif nargin > 2
+                error([newline 'MI_KSG_sim_manager >> Too many constructor arguments!']);
             else
+                obj.par_mode = true;
                 obj.verbose = 0;
             end
+            
             obj.sim_params = {};
             obj.sim_data = [];
+            
+            % determine number of available cores
+            c = parcluster('local');
+            obj.thread_count = c.NumWorkers;
+            
+            % ===== ===== ===== ===== =====
+            % Should a line be added to automatically determine whether it
+            % will be more efficient to run the simulations in serial or in
+            % parallel?
+            
         end
         
         function idx = add_sim(obj, x, y, k)
@@ -30,9 +50,24 @@ classdef MI_KSG_sim_manager
             obj.sim_data = nan(size(obj.sim_params,1),1); % initiate results arrays
             
             % iterate through all mutual information parameters in parallel
-            for i=1:size(obj.sim_params,1)
-                obj.sim_data(i) = MIxnyn(obj.sim_params(i,1), obj.sim_params(i,2), obj.sim_params(i,3));
+            if obj.par_mode > 0
+                % set up memory and variable management for parfor
+                tmp_sim_data = obj.sim_data;
+                xs = obj.sim_params(:,1);
+                ys = obj.sim_params(:,2);
+                ks = obj.sim_params(:,3);
+                
+                parfor i=1:size(obj.sim_params,1)
+                    tmp_sim_data(i) = MIxnyn(xs, ys, ks);
+                end
+            else
+                % if few sims to run, run in serial to avoid overhead of
+                % setting up parallel pool
+                for i=1:size(obj.sim_params,1)
+                        obj.sim_data(i) = MIxnyn(obj.sim_params(i,1), obj.sim_params(i,2), obj.sim_params(i,3));
+                end
             end
+            obj.sim_data = tmp_sim_data;
             r = true;
         end
     end
