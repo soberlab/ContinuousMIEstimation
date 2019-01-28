845dc674b02d32542b1cf87dae803a44faf92a56
neuRowsATL <bchung4@student.gsu.edu>
2019-01-28 12:04:16 -0500

    updated example script to reflect class updates

--- MI_KSG_EXAMPLE.m
+++ MI_KSG_EXAMPLE.m
@@ -1,10 +1,26 @@
+% Load spike times
 load('bl21lb21_171218_spikedata-0002-CH5.mat');
 ts = spikedata.ts(:,1)*1000;
-isi = diff(ts);
 
-sim_manager = MI_KSG_sim_manager;
+% Load pressure data
 
-core1 = MI_KSG_core(sim_manager, isi(1:end-1), isi(2:end), [3 4 5], -1);
 
-run_sims(sim_manager);
+% Instantiate data object
+neural_data = MI_KSG_data(30000,30000);
+add_spikes(neural_data, ts);
+
+% Instantiate analysis object(s)
+MI_isi = analysis_ISI1_ISI2(neural_data, [1]);
+
+buildMIs(MI_isi);
+
+% --> update k-values, etc.
+
+calcMIs(MI_isi);
+
+%sim_manager = MI_KSG_sim_manager;
+
+%core1 = MI_KSG_core(sim_manager, isi(1:end-1), isi(2:end), [3 4 5], -1);
+
+%run_sims(sim_manager);
 
