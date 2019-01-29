714b9ecf2ae9ff5c3a14c5f66bb294e51f91fc95
neuRowsATL <bchung808@gmail.com>
2019-01-29 12:04:53 -0500

    Update and rename analysis_timing_behavior.m to mi_timing_behav.m

--- mi_timing_behav.m
+++ mi_timing_behav.m
@@ -1,4 +1,4 @@
-classdef analysis_timing_behavior < MI_KSG_data_analysis
+classdef mi_timing_behav < MI_KSG_data_analysis
     %Each of these objects sets the stage to calculate the mutual
     %information between spike count and behavior and stores the results of
     %the calculation. 
@@ -8,7 +8,7 @@ classdef analysis_timing_behavior < MI_KSG_data_analysis
     end
     
     methods
-       function obj = analysis_timing_behavior(objData,var1,var2, verbose)
+       function obj = mi_timing_behav(objData,var1,var2, verbose)
             % var1- positive integer (neuron number)
             % var2- -1 (indicating pressure)
             obj =  MI_KSG_data_analysis(objData, var1, var2);
