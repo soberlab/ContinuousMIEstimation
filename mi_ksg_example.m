21505a83cfc3727362c4d9cf41a254a8a2975c33
neuRowsATL <bchung4@student.gsu.edu>
2019-01-29 15:45:15 -0500

    updated to include optimal k selection and plotting

--- mi_ksg_example.m
+++ mi_ksg_example.m
@@ -20,6 +20,7 @@ buildMIs(MI_isi);
 % --> update k-values, etc.
 
 MI_isi.arrMIcore{1}.opt_k = 0;
+MI_isi.arrMIcore{1}.k_values = 3:8;
 
 % Run mutual information calculations
 calcMIs(MI_isi);
@@ -36,18 +37,27 @@ for i=1:length(core1.k_values)
     plot_data_fraction(viz, core1, core1.k_values(i));
 end
 
+if size(core1.mi_data,1) > length(core1.k_values)
+    a = core1.k_values;
+    b = 1:10;
 
-a = core1.k_values;
-b = 1:10;
+    m = cell2mat(core1.mi_data(:,1));
+    c = reshape(m, length(b), length(a));
 
-m = cell2mat(core1.mi_data(:,1));
-c = reshape(m, length(b), length(a));
+    figure();
+    % plot mi surface
+    surf(a, b, c, 'FaceAlpha', 0.5);
+    hold on;
+    
+    % plot mi along data fracs = 1
+    plot3(core1.k_values, ones(1,length(core1.k_values)), c(1,:), 'k-', 'LineWidth', 5);
 
-figure();
-surf(a, b, c, 'FaceAlpha', 0.5);
-hold on;
-plot3(core1.k_values, ones(1,length(core1.k_values)), c(1,:), 'k-', 'LineWidth', 5);
-
-xlabel('k-value');
-ylabel('Data Fraction (1/N)');
-zlabel('MI (bits)');
+    % plot optimal k-value
+    k_ix = core1.k_values == core1.opt_k;
+    k_ixs = [core1.mi_data{:,4}] == core1.opt_k;
+    plot3(ones(1,length(b))*core1.opt_k, 1:10, c(:,k_ix), 'r-', 'LIneWidth', 5);
+    
+    xlabel('k-value');
+    ylabel('Data Fraction (1/N)');
+    zlabel('MI (bits)');
+end
